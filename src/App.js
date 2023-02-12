f9a2b98cfd9bdf48e1312aa83ff0d28b0337c0d2
watanabe12 <o8o6o191108@gmail.com>
2023-02-13 01:11:53 +0900

    複数の状態変化を持たせた実装

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const App = () => {
+const App = props => {
+
+// props適用前
+// const App = () => {
+//   const initialStatus = {
+//     name: '',
+//     price:1000
+//   }
+
+// const [name, setName] = useState(initialStatus.name)
+// const [price, setPrice] = useState(initialStatus.price)
+
+  // props適用後
+  const [name, setName] = useState(props.name)
+  const [price, setPrice] = useState(props.price)
+
+  const reset = () => {
+    setPrice(props.price)
+    setName(props.name)
+  }
+
   return (
-    <div>
-      this is template
-    </div>
+    <>
+      <p>現在の{name}は{price}円です。</p>
+      <button onClick={ () => setPrice(price + 1) }>+1</button>
+      <button onClick={ () => setPrice(price - 1) }>-1</button>
+      <button onClick={reset}>reset</button>
+      <input value={name} onChange={event => setName(event.target.value)}/>
+    </>
   )
+}
+
+// props設定の適用
+App.defaultProps = {
+  name: '',
+  price: 1000
 }
 
 export default App
