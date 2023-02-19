2b0a8239c8689dbc8ebe0bf357c57d4382faf2f7
watanabe12 <o8o6o191108@gmail.com>
2023-02-19 20:59:38 +0900

    implement object state

--- src/App.js
+++ src/App.js
@@ -2,32 +2,30 @@ import React, { useState } from 'react'
 
 const App = props => {
 
-// props適用前
-// const App = () => {
-//   const initialStatus = {
-//     name: '',
-//     price:1000
-//   }
 
-// const [name, setName] = useState(initialStatus.name)
-// const [price, setPrice] = useState(initialStatus.price)
+  const [state, setState] = useState(props)
+  // stateの分割代入でstateの記述を短縮
+  const {name , price } = state
 
-  // props適用後
-  const [name, setName] = useState(props.name)
-  const [price, setPrice] = useState(props.price)
+  // return (
+  //   <>
+  //     <p>現在の{state.name}は{state.price}円です。</p>
+  //     <button onClick={ () => setState({...state, price: state.price + 1}) }>+1</button>
+  //     <button onClick={ () => setState({...state, price: state.price - 1}) }>-1</button>
+  //     <button onClick={ () => setState(props) }>reset</button>
+  //     <input value={state.name} onChange={e => setState({...state, name: e.target.value})}/>
+  //   </>
+  // )
 
-  const reset = () => {
-    setPrice(props.price)
-    setName(props.name)
-  }
 
+  // onClickの中が変数だった物をオブジェクトで持たせる
   return (
     <>
       <p>現在の{name}は{price}円です。</p>
-      <button onClick={ () => setPrice(price + 1) }>+1</button>
-      <button onClick={ () => setPrice(price - 1) }>-1</button>
-      <button onClick={reset}>reset</button>
-      <input value={name} onChange={event => setName(event.target.value)}/>
+      <button onClick={ () => setState({...state, price: price + 1}) }>+1</button>
+      <button onClick={ () => setState({...state, price: price - 1}) }>-1</button>
+      <button onClick={ () => setState(props) }>reset</button>
+      <input value={name} onChange={e => setState({...state, name: e.target.value})}/>
     </>
   )
 }
